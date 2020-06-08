b7145fcd194634643d1559c6803be7ebbdc93048
Pierre Guillot <50145663+pierre-guillot-sonarsource@users.noreply.github.com>
2020-06-08 16:56:39 +0200

    Update file.py

--- file.py
+++ file.py
@@ -1,3 +1,6 @@
 import logging
 from logging import Logger, Handler, Filter
 from logging.config import fileConfig, dictConfig
+
+filename = tempfile.mktemp() # Noncompliant
+tmp_file = open(filename, "w+")
